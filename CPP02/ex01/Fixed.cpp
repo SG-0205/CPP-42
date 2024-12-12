fae55c45df29070503b2652bbda500cddecacbe7
SG-0205 <samgold0205@gmail.com>
2024-12-12 14:16:12 +0100

    ex01 OK

--- CPP02/ex01/Fixed.cpp
+++ CPP02/ex01/Fixed.cpp
@@ -1,25 +1,38 @@
 #include "Fixed.hpp"
+#include <climits>
+#include <cmath>
+#include <cstdlib>
+#include <iomanip>
 #include <iostream>
 #include <ostream>
+#include <stdexcept>
 
-const int Fixed::_decimals_bit_count = 8;
+std::ostream &operator<<(std::ostream &output, const Fixed &number) {
+  int raw_bits = number.getRawBits();
+  int scale_factor = number.getScaleFactor();
+  output << (raw_bits / scale_factor) +
+                static_cast<float>(raw_bits % scale_factor) / scale_factor;
+  return (output);
+}
+
+const int Fixed::_bit_scale = 8;
 
 void Fixed::_writeLog(Fixed::log_type type) const {
   switch (type) {
   case (DEF_CONSTRUCTOR):
     std::cout << "Default constructor called";
     break;
+  case (INT_CONSTRUCTOR):
+    std::cout << "Int constructor called";
+    break;
+  case (FLOAT_CONSTRUCTOR):
+    std::cout << "Float constructor called";
+    break;
   case (COPY_CONSTRUCTOR):
     std::cout << "Copy constructor called";
     break;
   case (COPY_ASSIGNEMENT):
     std::cout << "Copy assignement operator called";
-    break;
-  case (GETRB):
-    std::cout << "getRawBits member function called";
-    break;
-  case (SETRB):
-    std::cout << "setRawBits member function called";
     break;
   case (DESTRUCTOR):
     std::cout << "Desctructor called";
@@ -30,9 +43,41 @@ void Fixed::_writeLog(Fixed::log_type type) const {
   std::cout << std::endl;
 }
 
+int Fixed::_scale_factor(void) const {
+  int result = 2;
+  int limit = _bit_scale;
+
+  while (limit-- > 0)
+    result *= 2;
+
+  return (result);
+}
+
+int Fixed::getScaleFactor(void) const { return (_scale_factor()); }
+
 Fixed::Fixed() {
   _value = 0;
   _writeLog(DEF_CONSTRUCTOR);
+}
+
+Fixed::Fixed(const int to_convert) {
+  if (to_convert * _scale_factor() >= INT_MAX ||
+      to_convert * _scale_factor() <= INT_MIN)
+    throw std::invalid_argument("Fixed(int to_convert): to_convert would "
+                                "overflow/underflow if converted.");
+  else
+    _value = _scale_factor() * to_convert;
+  _writeLog(INT_CONSTRUCTOR);
+}
+
+Fixed::Fixed(const float to_convert) {
+  if (to_convert * _scale_factor() >= INT_MAX ||
+      to_convert * _scale_factor() <= INT_MIN)
+    throw std::invalid_argument("Fixed(float to_convert): to_convert would "
+                                "overflow/underflow if converted.");
+  else
+    _value = _scale_factor() * to_convert;
+  _writeLog(FLOAT_CONSTRUCTOR);
 }
 
 Fixed::Fixed(Fixed const &other) {
@@ -44,17 +89,15 @@ Fixed::Fixed(Fixed const &other) {
 
 void Fixed::operator=(const Fixed &other) {
   _writeLog(COPY_ASSIGNEMENT);
-  this->_value = other.getRawBits();
+  setRawBits(other.getRawBits());
 }
 
-int Fixed::getRawBits(void) const {
-  _writeLog(GETRB);
-  return (_value);
-}
+float Fixed::toFloat(void) const { return (_value / _scale_factor()); }
 
-void Fixed::setRawBits(const int raw) {
-  _writeLog(SETRB);
-  _value = raw;
-}
+int Fixed::toInt(void) const { return (_value / _scale_factor()); }
+
+int Fixed::getRawBits(void) const { return (_value); }
+
+void Fixed::setRawBits(const int raw) { _value = raw; }
 
 Fixed::~Fixed() { _writeLog(DESTRUCTOR); }
--- CPP02/ex01/Fixed.hpp
+++ CPP02/ex01/Fixed.hpp
@@ -1,27 +1,37 @@
 #pragma once
 
+#include <ostream>
 class Fixed {
 public:
   Fixed();
+  Fixed(const int to_convert);
+  Fixed(const float to_convert);
   Fixed(const Fixed &other);
+
   void operator=(Fixed const &other);
 
   int getRawBits(void) const;
+  float toFloat(void) const;
+  int toInt(void) const;
   void setRawBits(const int raw);
+  int getScaleFactor(void) const;
 
   ~Fixed();
 
 private:
   int _value;
-  static const int _decimals_bit_count;
+  static const int _bit_scale;
 
   enum log_type {
     DEF_CONSTRUCTOR,
+    INT_CONSTRUCTOR,
+    FLOAT_CONSTRUCTOR,
     COPY_CONSTRUCTOR,
     COPY_ASSIGNEMENT,
-    GETRB,
-    SETRB,
     DESTRUCTOR
   };
   void _writeLog(log_type type) const;
+  int _scale_factor(void) const;
 };
+
+std::ostream &operator<<(std::ostream &output, const Fixed &number);
